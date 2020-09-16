daf55e87447ba0f8ba0b1ee449f3d32b903c272f
Evgeny Ovodkov <kefir4ek@gmail.com>
2020-09-16 23:28:02 +0300

    script refactoring

--- js/script.js
+++ js/script.js
@@ -10,10 +10,25 @@ const personalMovieDB = {
 	privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-			b = prompt('На сколько оцените его?', ''),
-			c = prompt('Один из последних просмотренных фильмов?', ''),
-			d = prompt('На сколько оцените его?', '');
+for (let index = 0; index < 2; index++) {
+	const a = prompt('Один из последних просмотренных фильмов?', ''),
+				b = prompt('На сколько оцените его?', '');
+	
+	if (a !== 'null' && b !== 'null' && a !== '' && b !== '' && a.length < 50) {
+		personalMovieDB.movies[a] = b;
+	} else {
+		index--;
+	}
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10) {
+	console.log('Просмотрено довольно мало фильмов!');
+} else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+	console.log('Вы классический зритель!');
+} else if (personalMovieDB.count >= 30) {
+	console.log('Вы киноман!');
+} else {
+	console.log('Произошла ошибка!');
+}
+
+console.log(personalMovieDB);
